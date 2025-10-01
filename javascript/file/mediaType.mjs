f959665937a8ed3ad88b84aa0e9f0be485943939
Kairianu <jakuss6@gmail.com>
2025-10-01 03:11:02 -0700

    Updated JavaScript media types

--- javascript/file/mediaType.mjs
+++ javascript/file/mediaType.mjs
@@ -1,20 +1,44 @@
+export const plainMediaType = 'text/plain';
+
+
+const jpegMediaType = 'image/jpeg';
+
+export const imageMediaTypes = {
+	gif: 'image/gif',
+	jpeg: jpegMediaType,
+	jpg: jpegMediaType,
+	png: 'image/png',
+	svg: 'image/svg+xml',
+};
+
+
+const htmlMediaType = 'text/html';
 const javascriptMediaType = 'text/javascript';
 
-export const extensionMediaTypes = {
+export const textMediaTypes = {
 	css: 'text/css',
-	html: 'text/html',
+	htm: htmlMediaType,
+	html: htmlMediaType,
 	js: javascriptMediaType,
 	json: 'application/json',
+	md: 'text/markdown',
 	mjs: javascriptMediaType,
-	plain: 'text/plain',
+	txt: plainMediaType,
 };
+
+
+export const extensionMediaTypes = {
+	...imageMediaTypes,
+	...textMediaTypes,
+};
+
 
 export const filePathExtensionRegExp = /[\\/]?.*\.(.+?)$/;
 
 
 
 export function getMediaTypeFromExtension(extension) {
-	return extensionMediaTypes[extension] || extensionMediaTypes.plain;
+	return extensionMediaTypes[extension] || plainMediaType;
 }
 
 export function getMediaTypeFromFilePath(filePath) {
