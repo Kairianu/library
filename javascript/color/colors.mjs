f0d4f33cef392bf191c74455f14515857c1fee86
Kairianu <jakuss6@gmail.com>
2025-11-02 04:29:41 -0800

    Added JavaScript color

---
+++ javascript/color/colors.mjs
@@ -0,0 +1,17 @@
+export const colors = {
+	dusk: 'hsl(231, 17%, 16%)',
+	forest: 'hsl(140, 67%, 22%)',
+	offWhite: 'hsl(0, 0%, 90%)',
+	sand: 'hsl(38, 42%, 45%)',
+};
+
+
+
+export const darkSea = {
+	background: {
+		base: colors.dusk,
+	},
+	foreground: {
+		base: colors.sand,
+	},
+};
