d07487d14523158fda036f7c52317aa670f68772
Kairianu <jakuss6@gmail.com>
2025-11-03 05:23:55 -0800

    Updated JavaScript colors

--- javascript/color/colors.mjs
+++ javascript/color/colors.mjs
@@ -1,11 +1,21 @@
 export const colors = {
 	dusk: 'hsl(231, 17%, 16%)',
 	forest: 'hsl(140, 67%, 22%)',
+	offBlack: 'hsl(0, 0%, 1%)',
 	offWhite: 'hsl(0, 0%, 90%)',
 	sand: 'hsl(38, 42%, 45%)',
 };
 
 
+
+export const dark = {
+	background: {
+		base: colors.offBlack,
+	},
+	foreground: {
+		base: colors.offWhite,
+	},
+};
 
 export const darkSea = {
 	background: {
