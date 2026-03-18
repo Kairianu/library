d204e71a0ca992caf7ffb8940d79ba67f240553c
Kairianu <jakuss6@gmail.com>
2026-03-18 05:42:52 -0700

    Updated JavaScript object

--- javascript/object/object.mjs
+++ javascript/object/object.mjs
@@ -1,14 +1,91 @@
 import * as functions from '../function/function.mjs';
 
 
-export function isObject(value) {
-	if ( value == null ) {
-		return false;
+export function convertToObject(value) {
+	if ( isObject(value) ) {
+		return value;
 	}
 
-	return typeof(value) == 'object';
+	if ( value == undefined ) {
+		return getNewObject();
+	}
+
+	return new Object(value);
+}
+
+export function getIterator(value, iterableMethod=Object.values) {
+	if ( isIterable(value) ) {
+		return value;
+	}
+
+	if ( value != undefined ) {
+		if ( typeof(iterableMethod) == 'function' ) {
+			return iterableMethod(value);
+		}
+	}
+
+	return [];
+}
+
+export function getNewObject() {
+	return {};
+}
+
+export function isAsyncIterable(value) {
+	const asyncIterator = value?.[Symbol.asyncIterator];
+
+	if ( typeof(asyncIterator) == 'function' ) {
+		return true;
+	}
+
+	return false;
+}
+
+export function isIterable(value) {
+	const iterator = value?.[Symbol.iterator];
+
+	if ( typeof(iterator) == 'function' ) {
+		return true;
+	}
+
+	return false;
+}
+
+export function isObject(value) {
+	if ( value != null ) {
+		if ( typeof(value) == 'object' ) {
+			return true;
+		}
+	}
+
+	return false;
 }
 
 export function shouldUseProperties(value) {
-	return isObject(value) || functions.isFunction(value);
+	if ( isObject(value) ) {
+		return true;
+	}
+
+	if ( functions.isFunction(value) ) {
+		return true;
+	}
+
+	return false;
+}
+
+export function validateIterable(value, loopAll) {
+	try {
+		if ( loopAll ) {
+			for ( const _ of value ) {}
+		}
+		else {
+			for ( const _ of value ) {
+				break;
+			}
+		}
+
+		return true;
+	} catch {}
+
+	return false;
 }
