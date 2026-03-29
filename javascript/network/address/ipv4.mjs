48f0e3dcc7e5db320671c711c3c0a430e914c8f9
Kairianu <jakuss6@gmail.com>
2026-03-29 15:12:55 -0700

    Updated JavaScript network address ipv4 library

--- javascript/network/address/ipv4.mjs
+++ javascript/network/address/ipv4.mjs
@@ -1,45 +1,39 @@
+import * as string from '../../primitives/string.mjs';
+
+
 export const hostAddress = '0.0.0.0';
 export const localhostAddress = '127.0.0.1';
 
+export const octetRegExpString = '25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9]';
 
-export function isValidAddress(value) {
-	if ( typeof(value) != 'string' ) {
-		return false;
-	}
+export const ipv4RegExpString = (
+	`(?<octet1>${octetRegExpString})`
+	+ `\\.(?<octet2>${octetRegExpString})`
+	+ `\\.(?<octet3>${octetRegExpString})`
+	+ `\\.(?<octet4>${octetRegExpString})`
+);
 
-	const addressArray = value.split('.');
+export const ipv4FullRegExp = new RegExp('^' + ipv4RegExpString + '$');
 
-	if ( addressArray.length != 4 ) {
-		return false;
-	}
 
-	for ( const octetString of addressArray ) {
-		if ( /[^\d]/.exec(octetString) ) {
-			return false;
-		}
-
-		const octet = parseInt(octetString);
-
-		if ( ! Number.isInteger(octet) ) {
-			return false;
-		}
-
-		if ( octet < 0 || octet > 255 ) {
-			return false;
-		}
-	}
-
-	return true;
-}
-
-export function validateAddress(value, defaultValue) {
+export function getValidAddress(value, fallbackValue) {
 	if ( isValidAddress(value) ) {
 		return value;
 	}
 
-	if ( isValidAddress(defaultValue) ) {
-		return defaultValue;
+	if ( isValidAddress(fallbackValue) ) {
+		return fallbackValue;
 	}
 
 	return hostAddress;
+}
+
+export function isValidAddress(value) {
+	if ( string.isString(value) ) {
+		if ( ipv4FullRegExp.test(value) ) {
+			return true;
+		}
+	}
+
+	return false;
 }
