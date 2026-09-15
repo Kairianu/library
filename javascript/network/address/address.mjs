52257743bf08902df283eb0580efcf32dbf9158f
Kairianu <174232877+Kairianu@users.noreply.github.com>
2026-09-15 02:17:49 -0700

    Created agnostic network address library

---
+++ javascript/network/address/address.mjs
@@ -0,0 +1,17 @@
+import * as ipv4 from './ipv4.mjs';
+import * as ipv6 from './ipv6.mjs';
+
+
+// TODO: Make this function more thorough.
+// TODO: Move to ipv6 library when more thorough.
+export function isIPv6Address(value) {
+	return String(value).includes(':');
+}
+
+export function getRoutableAddress(value) {
+	if ( isIPv6Address(value) ) {
+		return ipv6.getRoutableAddress(value);
+	}
+
+	return ipv4.getRoutableAddress(value);
+}
