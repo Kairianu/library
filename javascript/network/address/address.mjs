54e249ba65c81077d8f7819e259d5018266128e3
Kairianu <174232877+Kairianu@users.noreply.github.com>
2026-09-25 00:38:14 -0700

    Added getHostAddress method to network address library

--- javascript/network/address/address.mjs
+++ javascript/network/address/address.mjs
@@ -1,3 +1,5 @@
+import * as string from '../../primitives/string.mjs';
+
 import * as ipv4 from './ipv4.mjs';
 import * as ipv6 from './ipv6.mjs';
 
@@ -6,6 +8,23 @@ import * as ipv6 from './ipv6.mjs';
 // TODO: Move to ipv6 library when more thorough.
 export function isIPv6Address(value) {
 	return String(value).includes(':');
+}
+
+export function getHostAddress(hostname, port) {
+	hostname = string.toString(hostname) ?? '';
+	port = string.toString(port) ?? '';
+
+	if ( isIPv6Address(hostname) ) {
+		hostname = '[' + hostname + ']';
+	}
+
+	let hostString = hostname;
+
+	if ( port ) {
+		hostString += ':' + port;
+	}
+
+	return hostString;
 }
 
 export function getRoutableAddress(value) {
